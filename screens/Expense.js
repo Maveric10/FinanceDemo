c8c8f447573d22f6d07a0d935e66a3a74b221cfb
Rajeev <rranjan.ra15@gmail.com>
2022-12-26 18:08:00 +0530

    added form dropdown components

--- screens/Expense.js
+++ screens/Expense.js
@@ -12,6 +12,8 @@ import CustomButton from "../components/CustomButton";
 import DatePicker from "../components/Datepicker";
 import LabelInput from "../components/LabelInput";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import SelectDropdown from "react-native-select-dropdown";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Expense = () => {
   // const [showCalendar, setShowCalendar] = useState(false);
@@ -26,7 +28,8 @@ const Expense = () => {
   const [formValue, setFormValue] = useState("");
   // const [list, setList] = useState([]);
   const [formData, setFormData] = useState("");
-
+  const expenseVar = ["Execute Expense", "Plan Expense"];
+  const expenseType = ["Pure Expense", "Investment", "Lump Sum"];
   // function submitForm() {
   //   setList([...list, formValue]);
   //   setFormValue(formValue);
@@ -73,8 +76,86 @@ const Expense = () => {
       />
       <CustomButton btnTitle="submit" handlePress={submitForm} /> */}
 
+      <SelectDropdown
+        data={expenseVar}
+        // defaultButtonText={"Execute Expense"}
+        defaultValueByIndex={0}
+        defaultValue={"Execute Expense"}
+        renderDropdownIcon={(isOpened) => {
+          return (
+            <Icon
+              name={isOpened ? "chevron-up" : "chevron-down"}
+              color={"#444"}
+              size={18}
+            />
+          );
+        }}
+        buttonStyle={styles.dropdownForm}
+        buttonTextStyle={styles.dropdownFormTextStyle}
+        dropdownStyle={styles.formDropdown}
+        rowTextStyle={styles.formDropdownRowText}
+        dropdownOverlayColor={"transparent"}
+      />
+      <SelectDropdown
+        data={expenseType}
+        defaultButtonText={"Pure Expense"}
+        defaultValue={"Pure Expense"}
+        // onSelect={(selectedItem, index) => {
+        //   console.log(selectedItem, index);
+        // }}
+        // buttonTextAfterSelection={(selectedItem, index) => {
+        //   return selectedItem;
+        // }}
+        // rowTextForSelection={(item, index) => {
+        //   return item;
+        // }}
+        renderDropdownIcon={(isOpened) => {
+          return (
+            <Icon
+              name={isOpened ? "chevron-up" : "chevron-down"}
+              color={"#444"}
+              size={18}
+            />
+          );
+        }}
+        buttonStyle={styles.dropdownForm}
+        buttonTextStyle={styles.dropdownFormTextStyle}
+        dropdownStyle={styles.formDropdown}
+        rowTextStyle={styles.formDropdownRowText}
+        dropdownOverlayColor={"transparent"}
+      />
       <TextInput
-        placeholder="Enter data to add"
+        placeholder="Date"
+        style={styles.inputContainer}
+        value={formValue}
+        onChangeText={(value) => setFormValue(value)}
+      />
+      <TextInput
+        placeholder="Expense Purpose"
+        style={styles.inputContainer}
+        value={formValue}
+        onChangeText={(value) => setFormValue(value)}
+      />
+      <TextInput
+        placeholder="Cost Assosciated"
+        style={styles.inputContainer}
+        value={formValue}
+        onChangeText={(value) => setFormValue(value)}
+      />
+      <TextInput
+        placeholder="Date"
+        style={styles.inputContainer}
+        value={formValue}
+        onChangeText={(value) => setFormValue(value)}
+      />
+      <TextInput
+        placeholder="For self"
+        style={styles.inputContainer}
+        value={formValue}
+        onChangeText={(value) => setFormValue(value)}
+      />
+      <TextInput
+        placeholder="Important and urgent"
         style={styles.inputContainer}
         value={formValue}
         onChangeText={(value) => setFormValue(value)}
@@ -83,7 +164,9 @@ const Expense = () => {
         style={styles.btnContainer}
         onPress={() => submitData()}
       >
-        <Text>Add Data</Text>
+        <Text style={{ fontSize: 15, fontWeight: "500", color: "white" }}>
+          Submit
+        </Text>
       </TouchableOpacity>
       {/* <View>
         {list.map((items, index) => {
@@ -103,14 +186,33 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     width: 300,
     padding: 10,
+    marginVertical: 5,
   },
   btnContainer: {
-    backgroundColor: "green",
+    backgroundColor: "#007bff",
     width: 300,
     color: "white",
     padding: 5,
     marginVertical: 10,
     alignItems: "center",
+  },
+  dropdownForm: {
+    borderWidth: 2,
+    borderColor: "blue",
+    width: 300,
+    marginVertical: 5,
+  },
+  dropdownFormTextStyle: {
+    textAlign: "left",
+    fontSize: 15,
+    fontWeight: "200",
+  },
+  formDropdown: {
+    width: 300,
+    borderRadius: 5,
+  },
+  formDropdownRowText: {
+    textAlign: "left",
   },
 });
 
